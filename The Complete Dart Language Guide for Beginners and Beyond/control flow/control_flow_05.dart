5787c5b10acb67316d6db70940d512ac3a34ab33
Saint. Clever <parchment@gmx.com>
2022-04-27 12:04:12 -0400

    update switch statement

--- The Complete Dart Language Guide for Beginners and Beyond/control flow/control_flow_05.dart
+++ The Complete Dart Language Guide for Beginners and Beyond/control flow/control_flow_05.dart
@@ -1,13 +1,28 @@
 void main() {
   const pos = 1;
 
-  if (pos == 1) {
-    print('gold 🤩');
-  } else if (pos == 2) {
-    print('silver 😀');
-  } else if (pos == 3) {
-    print('bronze 🙂');
-  } else {
-    print('no medal, try again 😢');
+  // if (pos == 1) {
+  //   print('gold 🤩');
+  // } else if (pos == 2) {
+  //   print('silver 😀');
+  // } else if (pos == 3) {
+  //   print('bronze 🙂');
+  // } else {
+  //   print('no medal, try again 😢');
+  // }
+
+  switch (pos) {
+    case 1:
+      print('gold 🤩');
+      break;
+    case 2:
+      print('silver 😀');
+      break;
+    case 3:
+      print('silver 🙂');
+      break;
+    default:
+      print('no medal, try again 😢');
+      break;
   }
 }
